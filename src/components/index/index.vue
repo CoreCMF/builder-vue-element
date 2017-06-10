5c85835b41cf2dcf6a276ea9a691691b22c5fcbe
bigrocs <bigrocs@qq.com>
2017-06-10 16:27:39 +0800

    获取当前路由数据

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -10,8 +10,14 @@ export default {
   created () {
 
   },
+  watch: {
+      $route: 'initData',
+  },
   methods: {
-
+    initData() {
+      let apiUrl = this.$route.meta.apiUrl
+      this.$store.dispatch('getData',{ apiUrl }) //获取当前路由数据
+    }
   }
 }
 </script>
