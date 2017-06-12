6fa3affec8d87d36883f17d53ec296a08b1092b8
bigrocs <bigrocs@qq.com>
2017-06-12 17:34:20 +0800

    添加tabs 初始化数据

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,23 +1,43 @@
 <template>
   <div class="layout-content-main">
-    builder-vue-element-main
+
+    <el-tabs type="card" v-if="data.tabs" v-model="tabIndex" @tab-click="handleTabsClick">
+      <el-tab-pane v-for="(tabs,key) in data.tabs" :key="key" :name="key.toString()" :label="tabs">
+        <bve-form v-if="data.form"/>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 export default {
   name: 'index',
+  data() {
+    return {
+      data: {
+        tabs:null,
+      },
+      tabIndex:null
+    }
+  },
   created () {
-
+    this.initData()
   },
   watch: {
-      $route: 'initData',
+    $route: 'initData',
   },
   methods: {
     initData() {
+      let _this = this
       let apiUrl = this.$route.meta.apiUrl
-      this.$store.dispatch('getData',{ apiUrl }) //获取当前路由数据
-    }
+      let thenFunction = function(Response) {
+        _this.data = Response.data
+      }
+      this.$store.dispatch('getData',{ apiUrl, thenFunction }) //获取当前路由数据
+    },
+    handleTabsClick(tab, event){
+        console.log(tab.name, event);
+    },
   }
 }
 </script>
