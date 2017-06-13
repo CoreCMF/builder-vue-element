a42a0258ea0f985e5d21b219ec96ff8e2765d420
bigrocs <bigrocs@qq.com>
2017-06-13 14:38:24 +0800

    传入form组件数据

---
+++ src/components/form/form.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-tabs v-if="data.tabs">
+      <el-tab-pane v-for="(tab,key) in data.tabs" :key="key" :name="key.toString()" :label="tab">
+          asd
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'form',
+  props: {
+      data: {
+          type: Object,
+          default: ''
+      },
+  },
+  created() {
+    this.initData();//初始化页面数据
+  },
+  methods: {
+    initData() {
+      console.log(this.data);
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
