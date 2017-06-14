8c616ffec6bcee0f617c1d7f64055475bbb5f74c
bigrocs <bigrocs@qq.com>
2017-06-14 16:35:24 +0800

    增加FormItemCheckbox

---
+++ src/components/form-item-checkbox/form-item-checkbox.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-form-item :label="config.label" :prop="config.name">
+      SADA
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: 'bve-form-item-checkbox',
+  props: {
+    data: {
+        type: '',
+        default: ''
+    },
+    config: {
+        type: Object,
+        default: ''
+    },
+  },
+  created() {
+    this.initData();//初始化页面数据
+  },
+  methods: {
+    initData() {
+        // console.log(this.config)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
