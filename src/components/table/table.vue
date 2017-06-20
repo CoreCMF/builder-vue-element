806ba27fa57e33adfa68ec05a75a149a664e318b
bigrocs <bigrocs@qq.com>
2017-06-20 09:26:56 +0800

    设置table显示渲染

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>
-table
+  <div class="table-item">
+    <el-tabs
+      v-if="data.tabs"
+      @tab-click="handleTabsClick"
+      v-model="index"
+    >
+      <el-tab-pane v-for="(tab,key) in data.tabs" :key="key" :name="key.toString()" :label="tab">
+        <bve-table-item  v-if="index == key" :data="data"/>
+      </el-tab-pane>
+    </el-tabs>
+    <bve-table-item :data="data" v-else/>
   </div>
 </template>
 
 <script>
 export default {
   name: 'bve-table',
-
+  props: {
+      data: {
+          type: Object,
+          default: ''
+      },
+      tabIndex:{}
+  },
+  computed: {
+    index() {
+      if (this.tabIndex) {
+        return this.tabIndex.toString()
+      }
+      return Object.keys(this.data.tabs)[0].toString()
+    }
+  },
+  methods: {
+    handleTabsClick(tab, event) {
+      this.$emit('tab-click',tab, event)
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
-
+  .table-item{
+    background: #fff;
+    overflow: hidden;
+    border-radius: 4px;
+    padding: 10px;
+  }
 </style>
