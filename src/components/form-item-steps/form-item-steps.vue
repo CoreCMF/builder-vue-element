65d89d80761999562f8d3e39537a17c9b828c5a3
bigrocs <bigrocs@qq.com>
2017-07-08 11:47:38 +0800

    steps over

--- src/components/form-item-steps/form-item-steps.vue
+++ src/components/form-item-steps/form-item-steps.vue
@@ -1,13 +1,12 @@
 <template>
 <el-steps
-  :space="100"
-  :active="0"
-  finish-status="success"
-  :center="true"
+  :space="space"
+  :active="active"
+  :finish-status="finishStatus"
+  :align-center="alignCenter"
+  :center="center"
 >
-  <el-step title="已完成"></el-step>
-  <el-step title="进行中"></el-step>
-  <el-step title="步骤 3"></el-step>
+  <el-step v-for="(title,key) in titles" :title="title" :key="key"></el-step>
 </el-steps>
 </template>
 
@@ -24,14 +23,29 @@ export default {
   computed: {
     currentValue: {
       get() {
-        if (this.value!=null) {
-           return this.value.toString()
-        }
-        return
+        return this.value.toString()
       },
       set(newValue) {
         this.$emit('input', newValue)
       }
+    },
+    titles() {
+      return this.config.title
+    },
+    center() {
+      return (this.config.center!=null)? this.config.center: true
+    },
+    alignCenter() {
+      return (this.config.alignCenter!=null)? this.config.alignCenter: true
+    },
+    finishStatus() {
+      return (this.config.finishStatus!=null)? this.config.finishStatus: 'success'
+    },
+    active() {
+      return (this.config.active!=null)? this.config.active: 0
+    },
+    space() {
+      return (this.config.space!=null)? this.config.space: null
     }
   }
 }
